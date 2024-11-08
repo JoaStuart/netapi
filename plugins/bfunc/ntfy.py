cca5b9f3ad09a30add9d3347208648e5f81f6b5b
Joa <joa@deininger.name>
2024-11-08 13:20:07 +0100

    NTFY Integration

---
+++ plugins/bfunc/ntfy.py
@@ -0,0 +1,18 @@
+import requests
+import config
+from device.api import APIFunct
+
+
+class Ntfy(APIFunct):
+    def api(self) -> dict | tuple[bytes, str]:
+        body = {
+            "topic": config.load_var("ntfy.default_topic"),
+            "title": "New notification!",
+        } | self.body
+
+        requests.post(
+            f"http://{config.load_var("ntfy.ip")}:{config.load_var("ntfy.port")}/",
+            json=body,
+        )
+
+        return {}
